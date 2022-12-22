3aec709584237f8222bd0b12911df3a34083761e
HAFSA BOUSSABOUN <hafsabousaboun2000@gmail.com>
2022-12-22 12:08:32 +0100

    second-version

--- irisDataSet.R
+++ irisDataSet.R
@@ -1,3 +1,5 @@
+##1 en utilisant les graphes deja installer dans R
+
 data("iris")
 ##consulter les données
 View(iris)
@@ -38,10 +40,56 @@ plot(pw,pl,col=sp)
 legend("topleft",levels(sp),fill = 1:3)
 
 
-##on teste la correlation ente pl et pw
+##on teste la correlation entre pl et pw
 cor(pw,pl)
-model<-lm(pl~pw)
+
+model<-lm(pw~pl)
 model
 plot(pl,pw,pch=8,col="red")
 speciesId<-as.numeric(iris$Species)
 speciesId
+plot(pl,pw,pch=speciesId,col=speciesId)
+plot(pl,pw,
+     pch=speciesId,
+     col=speciesId,
+     xlab = "petal megth (cm)",
+     ylab = "Petal width (cm)",
+     main = "Petal width vs. length"
+  
+)
+
+abline(lm(pw~pl))
+##calculer la correlation
+pcc<-cor(pw,pl)
+pcc<-round(pcc,2)
+paste("R=",pcc)
+
+
+text(5,0.5,paste("R=",pcc))
+legend("topleft",
+       levels(iris$Species),
+       pch=1:3,
+       col=1:3)
+
+
+ma<-as.matrix(iris[,1:4]) ##convert to matrix
+colMeans(ma)
+colSums(ma)
+pairs(ma)
+pairs(ma,col=rainbow(3)[speciesId])
+
+##-------------------
+##install the package ggplot2
+install.packages("ggplot2")
+##load the ggplot2 package
+library(ggplot2) 
+## define a canvas
+ggplot(data=iris)+aes(x=Petal.Length,y=Petal.Width)+
+  geom_point(aes(color=Species,shape=Species))+
+  geom_smooth(method = lm)+
+  annotate("text",x=5,y=0.5,label="R=0.96")+ ##annotate with taxt
+  ##xlab("petal length (cm)"), ##x-axis labels
+  ##ylab=("pental width (cm)"), ##y-axis labels
+  ##ggtitle("correlation between petal length and width") #title
+
+
